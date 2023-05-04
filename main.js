65036307cacd3402a5a79fa38cfb45916964a2d8
Diptanu12 <129202457+Diptanu12@users.noreply.github.com>
2023-05-04 16:00:10 +0530

    Initial

---
+++ main.js
@@ -0,0 +1,35 @@
+
+
+
+//for login button
+
+function func1() {
+    var nm = document.getElementById("exampleInputEmail1").value;
+    var ag = document.getElementById("exampleInputPassword1").value;
+
+    console.log("Email  :" + nm);
+    console.log("Password :" + ag);
+}
+
+
+
+//for signup button
+
+function func2() {
+    var t = document.getElementById("exampleInputName").value;
+    var u = document.getElementById("eexampleInputEmail1").value;
+    var v = document.getElementById("exampleInputPhone").value;
+    var w = document.getElementById("exampleInputPassword12").value;
+    var x = document.getElementById("exampleInputPassword13").value;
+    
+
+    console.log("Name  :" + t);
+    console.log("Email Id :" + u);
+    console.log("phone Number :" + v);
+    console.log("Password :" + w);
+    console.log("Confirm Password :" + x);
+}
+
+
+
+
